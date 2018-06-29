3486955c77a77072fee3d0976bbb19f4ad7ecc67
Kris Helenek <khelenek@gmail.com>
2018-06-29 14:20:03 -0500

    allow for more flexibility

--- ga.py
+++ ga.py
@@ -25,7 +25,9 @@ from pprint import pprint
 from oauth2client import client
 from oauth2client import file
 from oauth2client import tools
+from pprint import pprint
 
+MINUTES_AGO_METRIC = "rt:minutesAgo"
 
 class GoogleAnalyticsCheck(AgentCheck):
   """ Collects as many metrics as instances defined in ga.yaml
@@ -43,69 +45,57 @@ class GoogleAnalyticsCheck(AgentCheck):
     profile = instance.get('profile')
     instanceTags = instance.get('tags')
     instanceTags.append("profile:" + profile)
-    ts = time.time() - 60
-    
-    
-    
-    # pageview collection
-    metricName = 'rt:pageviews'
-    pageviewsDims = ['rt:minutesAgo']
-    confDims = instance.get('pageview_dimensions');
-    if isinstance(confDims, list):
-      pageviewsDims = pageviewsDims + confDims
-    result = self.get_results(profile, metricName, pageviewsDims)
+    metricType = instance.get('metricType') or 'gauge'
+    metricName = instance.get('metricName')
+    gaMetricName = instance.get('gaMetricName')
+    gagaMetricName = instance.get('gagaMetricName')
+    dimensions = instance.get('dimensions')
+    self.log.info("Getting metric {0} with dimensions {1}".format(gaMetricName, dimensions))
+    result = self.get_results(profile, gaMetricName, dimensions)
     headers = result.get('columnHeaders')
     rows = result.get('rows')
-    
+
+    print result
+
     if len(rows) < 1:
+      print "No rows returned, no metrics sent"
       return
       
-    pvMetricsSent = 0
+    metricsSent = 0
+    minutesAgoMetric = MINUTES_AGO_METRIC in dimensions
+    minutesAgoIndex = -1
+    if (minutesAgoMetric):
+      minutesAgoColumn = filter(lambda header: header["name"] == MINUTES_AGO_METRIC, headers)[0]
+      minutesAgoIndex = headers.index(minutesAgoColumn)
+      ts = time.time() - 60
+      print "minutesAgoIndex found to be {0}".format(minutesAgoIndex)
+    else:
+      ts = time.time()
 
     for row in rows:
       # In order to have a consistent metric, we look for the value 1 minute ago
       # and not during the last minute.
-      if int(row[0]) == 1:
-        tags = []
-        tags.extend(instanceTags)
-        for i in xrange(len(headers)-1):
-          if i > 0:
-            # we remove the "rt" from the dimension name
-            tags.append(headers[i].get('name')[3:] + ":" + row[i])
-            
-        self.gauge("googleanalytics.rt.pageviews", 
-          row[len(row)-1], 
-          tags,
-          None,
-          None,
-          ts
-        )
-        
-        pvMetricsSent = pvMetricsSent + 1
-        
-    self.log.info("Pageview Metrics sent %s" % pvMetricsSent);
-        
+      if minutesAgoMetric and int(row[minutesAgoIndex]) != 1:
+        self.log.info("Skipping row because minutesAgo dimension is {0} (only checking 1)".format(row[minutesAgoIndex]))
+        continue
 
-
-    # activeUsers collection
-    metricName = 'rt:activeUsers'
-    activeuserDims = []
-    tags = []
-    tags.extend(instanceTags)
-    
-    result = self.get_results(profile, metricName, activeuserDims);
-
-    activeUsers = int(result.get("totalsForAllResults").get(metricName))
-    
-    self.gauge("googleanalytics.rt.activeUsers", 
-          activeUsers, 
-          tags,
-          None,
-          None,
-          ts
-        )    
+      tags = []
+      tags.extend(instanceTags)
+      for i in xrange(len(headers)-1):
+        if i != minutesAgoIndex:
+          # we remove the "rt" from the dimension name
+          tags.append(headers[i].get('name')[3:] + ":" + row[i])
+          
+      if (metricType == "gauge"):
+        self.gauge(metricName, int(row[-1]), tags, None, None)
+      elif (metricType == "count"):
+        self.count(metricName, int(row[-1]), tags, None, None)
+      else:
+        print "UNKNOWN METRIC TYPE"
+      
+      metricsSent = metricsSent + 1
         
-    self.log.info("Active users %s" % activeUsers);
+    self.log.info("{0} sent: {1}".format(gaMetricName, metricsSent));        
 
   def __init__(self, *args, **kwargs):
     AgentCheck.__init__(self, *args, **kwargs)
@@ -123,7 +113,6 @@ class GoogleAnalyticsCheck(AgentCheck):
   def get_service(self, api_name, api_version, scope, key_file_location, service_account_email):
     credentials = ServiceAccountCredentials.from_json_keyfile_name(key_file_location, scopes=scope)
     http = credentials.authorize(httplib2.Http())
-
     # Build the service object.
     service = build(api_name, api_version, http=http)
     return service
